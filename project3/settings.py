706f18342b82cf1e41c83949534f28866cb6f938
Thom Zolghadr <thom@thomcodes.com>
2022-05-06 12:53:00 -0700

    reset settings

--- project3/settings.py
+++ project3/settings.py
@@ -31,7 +31,7 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['mail-thom.herokuapp.com', '127.0.0.1']
 CORS_ALLOWED_ORIGINS = [
-    'https://mail-thom.herokuapp.com', "http://localhost:3000", "http://127.0.0.1:8000", ]
+    'https://mail-thom.herokuapp.com', "http://localhost:3000", "http://127.0.0.1:3000", ]
 
 # Application definition
 
