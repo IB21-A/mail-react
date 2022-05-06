39b77cb6aaacd6106ca2ac53d2c56b190a367eec
Thom Zolghadr <thom@thomcodes.com>
2022-05-06 12:36:55 -0700

    Seperate mail urls into separate api route from main application

--- mail/urls.py
+++ mail/urls.py
@@ -11,21 +11,21 @@ urlpatterns = [
     #     path("register", views.register, name="register"),
 
     # API Routes
-    path("emails", views.compose, name="compose"),
-    path("emails/<int:email_id>", views.email, name="email"),
-    path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
-    path("api/test", views.api_test, name="api_test"),
-    path("api/register/", views.register, name="register"),
-    path("api/emails/compose/", views.compose, name="compose"),
-    path("api/emails/edit/<int:email_id>", views.email, name="email"),
-    path("api/emails/get/<int:email_id>", views.email, name="email"),
-    path("api/emails/<str:mailbox>/", views.mailbox, name="mailbox"),
-    path("api/emails/delete/<int:email_id>",
+    #     path("emails", views.compose, name="compose"),
+    #     path("emails/<int:email_id>", views.email, name="email"),
+    #     path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
+    path("test", views.api_test, name="api_test"),
+    path("register/", views.register, name="register"),
+    path("emails/compose/", views.compose, name="compose"),
+    path("emails/edit/<int:email_id>", views.email, name="email"),
+    path("emails/get/<int:email_id>", views.email, name="email"),
+    path("emails/<str:mailbox>/", views.mailbox, name="mailbox"),
+    path("emails/delete/<int:email_id>",
          views.delete_email, name="delete_email"),
 
     # Token Routes
-    path('api/token/', views.MyTokenObtainPairView.as_view(),
+    path('token/', views.MyTokenObtainPairView.as_view(),
          name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(),
          name='token_refresh'),
 ]
--- project3/urls.py
+++ project3/urls.py
@@ -16,13 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('mail.urls')),
+    path('api/', include('mail.urls')),
     re_path(r".*", TemplateView.as_view(template_name='index.html')),
 ]
