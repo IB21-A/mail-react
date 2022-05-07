a1a179f7233cb06775f904727e47f20ee8e33ae5
Thom Zolghadr <thom@thomcodes.com>
2022-05-06 20:29:18 -0700

    Clean up unused routes

--- mail/urls.py
+++ mail/urls.py
@@ -6,14 +6,7 @@ from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
 
-    #     path("", views.index, name="index"),
-    #     path("logout", views.logout_view, name="logout"),
-    #     path("register", views.register, name="register"),
-
     # API Routes
-    #     path("emails", views.compose, name="compose"),
-    #     path("emails/<int:email_id>", views.email, name="email"),
-    #     path("emails/<str:mailbox>", views.mailbox, name="mailbox"),
     path("test", views.api_test, name="api_test"),
     path("register/", views.register, name="register"),
     path("emails/compose/", views.compose, name="compose"),
