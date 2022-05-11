ba855113bdede6815dec51f3ae7adb6b47eaa325
Thom Zolghadr <thom@thomcodes.com>
2022-05-10 19:34:04 -0700

    Update autopopulate method

--- mail/newUserEmails.py
+++ mail/newUserEmails.py
@@ -9,23 +9,8 @@ def populateNewUserMailbox(newUser):
     users.add(newUser)
     users.add(thom)
 
-    subject = "Wow! Such a cool app!"
-    body = "Hey Thom,\nYou did some really cool stuff here! I love that when you respond to a message, it doesn't fill in tons of \"Re: Re: Re: Re: \" but just has one. Did you use Regular Expressions for that?\nPretty cool that messages are marked as read with a different color in the inbox, and I can mark them as unread again from the message. I love that I can see my sent messages too! You did a great job, I can't wait to hire you ;)\nBest,\nThe Hiring Manager"
-
-    for user in users:
-        email = Email(
-            user=user,
-            sender=newUser,
-            subject=subject,
-            body=body,
-            read=user == newUser
-        )
-        email.save()
-        email.recipients.add(thom)
-        email.save()
-
     subject = "Welcome to the Webmail application!"
-    body = "Hey there!\nThanks for stopping by and checking out my application. Feel free to poke around. Reply to this email and let me know you were here!\nBest,\nThom"
+    body = "Hey there!\n\nThanks for stopping by and checking out my application. Feel free to poke around. Reply to this email and let me know you were here!\n\nBest,\nThom"
 
     for user in users:
         email = Email(
@@ -37,6 +22,21 @@ def populateNewUserMailbox(newUser):
         )
         email.save()
         email.recipients.add(newUser)
+        email.save()
+
+    subject = "Wow! Such a cool app!"
+    body = "Hey Thom,\n\nYou did some really cool stuff here! I love that when you respond to a message, it doesn't fill in tons of \"Re: Re: Re: Re: \" but just has one. Did you use Regular Expressions for that?\n\nPretty cool that messages are marked as read with a different color in the inbox, and I can mark them as unread again from the message. I love that I can see my sent messages too! You did a great job, I can't wait to hire you ;)\n\nBest,\nThe Hiring Manager"
+
+    for user in users:
+        email = Email(
+            user=user,
+            sender=newUser,
+            subject=subject,
+            body=body,
+            read=user == newUser
+        )
+        email.save()
+        email.recipients.add(thom)
         email.save()
 
     # Archived Email
